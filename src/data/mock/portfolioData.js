71c64fd141a3f8767e1ff76c3e3345a120ec5887
quanting56 <qwerdgt@gmail.com>
2025-12-08 16:26:45 +0800

    chore(mock): 調整assetsMockData.js、portfolioData.js、mockData0050.js等mock data內容

--- src/data/mock/portfolioData.js
+++ src/data/mock/portfolioData.js
@@ -3,35 +3,35 @@ export const mockData = [
     id: null,
     name: "現金",
     shares: 1,
-    price: 32634,
-    cost: 32634,
+    price: 16880,
+    cost: 16880,
   },
   {
     id: "2330.TW",   // 股號
     name: "台積電",   // 股票名稱
-    shares: 167,     // 持有股數
-    price: 1450,     // 現價
-    cost: 899.31,    // 成本
+    shares: 100,     // 持有股數
+    price: 1460,     // 現價
+    cost: 688,       // 成本
   },
   {
     id: "00757.TW",
     name: "統一FANG+",
-    shares: 934,
+    shares: 757,
     price: 116,
     cost: 100
   },
   {
-    id: "00675L.TW",
-    name: "富邦臺灣加權正二",
-    shares: 5835,
-    price: 130.2,
-    cost: 81.01
+    id: "0050.TW",
+    name: "元大台灣50",
+    shares: 675,
+    price: 63.65,
+    cost: 50
   },
   {
     id: "00713.TW",
     name: "元大台灣高息低波",
-    shares: 422,
-    price: 51.4,
-    cost: 53.51
+    shares: 713,
+    price: 50.55,
+    cost: 47
   }
 ];
